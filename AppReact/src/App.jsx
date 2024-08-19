ede7b9b6bbda3b7de4c3cd8b492986a751b88980
Ulfserkir <m.ortega.col07@gmail.com>
2024-08-19 22:27:25 +0000

    Imported Fragment to repeat component on App.jsx

--- AppReact/src/App.jsx
+++ AppReact/src/App.jsx
@@ -1,7 +1,13 @@
 import Message from './component/Message'
+import { Fragment } from 'react';
 
 function App(){
-  return <Message />
+  return (
+  <>
+    <Message />
+    <Message />
+  </>
+  ); 
 }
 
 export default App;
