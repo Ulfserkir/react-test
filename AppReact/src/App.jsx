ac80bbd3e83084005c2ace60dc80d810f660fb73
Ulfserkir <m.ortega.col07@gmail.com>
2024-08-19 22:58:51 +0000

    Added component City to App and applied parameters

--- AppReact/src/App.jsx
+++ AppReact/src/App.jsx
@@ -7,7 +7,10 @@ function App(){
   return (
   <>
     <Header />
-    <City />
+    <City 
+    nombre="Lima"
+    descripcion="La capital del Perú"
+    imagen="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWtvF1qhmrNPU6oFO7_CowidpLZSrJV7iQJK-tIZUUhUk8DYp0SYozxl1VxR47vhIE0_Y&usqp=CAU"/>
     <Footer />
   </>
   ); 
--- AppReact/src/component/City.jsx
+++ AppReact/src/component/City.jsx
@@ -5,9 +5,9 @@ function City ( {nombre, descripcion, imagen} ){
     return (
         <>
         <div className="ciudad">
-            <h3>{nombre}</h3>
-            <p>{descripcion}</p>
-            <img src="{imagen}" alt="{nombre}" />
+            <h3 className="ciudad_titulo">{nombre}</h3>
+            <p className="ciudad_descripcion">{descripcion}</p>
+            <img src={imagen} alt={nombre} className="ciudad_imagen" />
         </div>
         </>
     );
