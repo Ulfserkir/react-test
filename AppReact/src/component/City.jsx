fbc458086418b7a98ebe7aeb9687784038622a1d
Ulfserkir <m.ortega.col07@gmail.com>
2024-08-19 22:46:28 +0000

    Created component with parameters> City

--- AppReact/src/component/City.jsx
+++ AppReact/src/component/City.jsx
@@ -1,9 +1,14 @@
 import { Fragment } from "react";
+import './CityStyle.css'
 
-function City (){
+function City ( {nombre, descripcion, imagen} ){
     return (
         <>
-        
+        <div className="ciudad">
+            <h3>{nombre}</h3>
+            <p>{descripcion}</p>
+            <img src="{imagen}" alt="{nombre}" />
+        </div>
         </>
     );
 }
